a33e952043fdd980dba22513381493b7f9def8f7
Megan Duclos <37349288+megsdev@users.noreply.github.com>
2018-06-09 09:55:24 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+let animals = [
+    {
+      name: 'Armadillo',
+      id: '1',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Nine-banded_Armadillo.jpg/1200px-Nine-banded_Armadillo.jpg'
+    },
+    {
+      name: 'Tiger',
+      id: '2',
+      imageUrl: 'http://kids.sandiegozoo.org/sites/default/files/2017-06/animal-hero-tiger_0.jpg'
+    }
+  ]
